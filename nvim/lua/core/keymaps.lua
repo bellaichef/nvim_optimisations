c6ca486949d0d3d0b0059744c2a93dee181ac1a3
bellaichef <45790724+bellaichef@users.noreply.github.com>
2023-01-06 10:55:49 +0100

    Add files via upload

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- Disable and Enable mouse in Vim
+vim.keymap.set('n', '<leader>dm', ':set mouse=<CR>')
+vim.keymap.set('n', '<leader>em', ':set mouse=a<CR>')
