c3f31283a640f141c9337d7eb0e6bc380d59b90c
bellaichef <45790724+bellaichef@users.noreply.github.com>
2023-01-08 19:07:58 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,23 @@
 require('core.keymaps')
 require('core.plugins')
-require('core.plugin_config')
+ 
+-- Check if packer has been configured
+local required_set = true
+packer_plugin_conf = vim.fn.stdpath 'config' .. '/plugin/packer_compiled.lua'
+ 
+if vim.fn.empty(vim.fn.glob(packer_plugin_conf)) > 0 then
+  required_set = false
+end
+ 
+if required_set then
+  require('core.plugin_config')
+else
+  print '=================================='
+  print '    Plugins are being installed'
+  print '         Press [ENTER] and'
+  print '    Wait until Packer completes,'
+  print '       then close nvim and '
+  print ' Installation process will go on !'
+  print '=================================='
+  return
+end
