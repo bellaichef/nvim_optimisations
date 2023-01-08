95cd0988c4fce3f986776ff739d247346620caa9
bellaichef <45790724+bellaichef@users.noreply.github.com>
2023-01-08 19:27:10 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,25 @@
 require('core.keymaps')
 require('core.plugins')
- 
+
 -- Check if packer has been configured
 local required_set = true
 packer_plugin_conf = vim.fn.stdpath 'config' .. '/plugin/packer_compiled.lua'
- 
+
 if vim.fn.empty(vim.fn.glob(packer_plugin_conf)) > 0 then
   required_set = false
 end
- 
+
 if required_set then
   require('core.plugin_config')
+
+  -- Check if LSPs are installed
+  local mason_not_set = false
+  mason_folder = vim.fn.stdpath 'data' .. '/mason/packages/intelephense/package.json'
+
+  if vim.fn.empty(vim.fn.glob(mason_folder)) > 0 then
+    vim.cmd('MasonInstall bash-language-server intelephense php-cs-fixer phpactor phpcs typescript-language-server')
+  end
+
 else
   print '=================================='
   print '    Plugins are being installed'
