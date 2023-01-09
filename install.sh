15829c9bfb194641dee8b7480c46aee14076e5b7
bellaichef <45790724+bellaichef@users.noreply.github.com>
2023-01-09 11:48:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,42 +6,42 @@ IFS=$'\n'
 requirements="OK"
 
 if [[ ! -f "/usr/bin/rg" ]];then
-	requirements_status="ripgrep missing"
+	requirements_status="/!\\ \033[1mripgrep missing\033[0m"
 	requirements="KO"
 else
 	requirements_status="ripgrep OK"
 fi
 
 if [[ $(nvim -v 2>&1 | grep "NVIM v" | cut -d'.' -f 2) -lt 8 || ! -f "/usr/bin/nvim" ]];then
-	requirements_status="$requirements_status\nNeoVim wrong version or missing (https://launchpad.net/~neovim-ppa/+archive/ubuntu/unstable)"
+	requirements_status="/!\\ \033[1m$requirements_status\nNeoVim wrong version or missing (https://launchpad.net/~neovim-ppa/+archive/ubuntu/unstable)\033[0m"
 	requirements="KO"
 else
 	requirements_status="$requirements_status\nNeoVim OK"
 fi
 
 if [[ $(node -v 2>&1 | cut -d'.' -f1 | tail -c 3) -lt 18 || ! -f "/usr/bin/node" ]];then
-	requirements_status="$requirements_status\nNode wrong version or missing (https://joshtronic.com/2022/04/24/how-to-install-nodejs-18-on-ubuntu-2004-lts/)"
+	requirements_status="/!\\ \033[1m$requirements_status\nNode wrong version or missing (https://joshtronic.com/2022/04/24/how-to-install-nodejs-18-on-ubuntu-2004-lts/)\033[0m"
         requirements="KO"
 else
         requirements_status="$requirements_status\nNode OK"
 fi
 
 if [[ ! -f "/usr/bin/npm" ]];then
-	requirements_status="$requirements_status\nnpm wrong version or missing (https://joshtronic.com/2022/04/24/how-to-install-nodejs-18-on-ubuntu-2004-lts/)"
+	requirements_status="/!\\ \033[1m$requirements_status\nnpm wrong version or missing (https://joshtronic.com/2022/04/24/how-to-install-nodejs-18-on-ubuntu-2004-lts/)\033[0m"
         requirements="KO"
 else
         requirements_status="$requirements_status\nnpm OK"
 fi
 
 if [[ $(php -v 2>&1 | head -n 1 | cut -d' ' -f2 | cut -d'.' -f1) -lt 8 || ! -f "/usr/bin/php" ]];then
-	requirements_status="$requirements_status\nPHP wrong version or missing https://linuxize.com/post/how-to-install-php-8-on-ubuntu-20-04/)"
+	requirements_status="/!\\ \033[1m$requirements_status\nPHP wrong version or missing https://linuxize.com/post/how-to-install-php-8-on-ubuntu-20-04/)/!\\ \033[0m"
         requirements="KO"
 else
         requirements_status="$requirements_status\nPHP OK"
 fi
 
 if [[ ! -f "/usr/bin/composer" ]];then
-	requirements_status="$requirements_status\ncomposer wrong version or missing (https://getcomposer.org/download/)"
+	requirements_status="/!\\ \033[1m$requirements_status\ncomposer wrong version or missing (https://getcomposer.org/download/)/!\\ \033[0m"
         requirements="KO"
 else
         requirements_status="$requirements_status\ncomposer OK"
@@ -53,7 +53,7 @@ if [[ $requirements == "OK" ]];then
 	echo -n "You are meeting the minimum requirements, Do you want to go on the the intallation ?  [(Y)/n]"
 	read -r runInstallation
 else
-	echo -n "You are NOT meeting the minimum requirements, Do you want to go on nvim_optimizationsthe the intallation anyway?  [(Y)/n]"
+	echo -e "/!\\ You are \033[1mNOT\033[0m meeting the minimum requirements, Do you want to go on with nvim_optimizationsthe the intallation anyway?  [(Y)/n]"
 	read -r runInstallation
 fi
 
