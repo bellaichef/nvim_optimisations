35ac4503262bbcae1359bdd878874b935730763f
bellaichef <45790724+bellaichef@users.noreply.github.com>
2023-01-11 09:17:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,42 +6,42 @@ IFS=$'\n'
 requirements="OK"
 
 if [[ ! -f "/usr/bin/rg" ]];then
-	requirements_status="/!\\ \033[1mripgrep missing\033[0m"
+	requirements_status="/!\\ \033[1mripgrep missing\033[0m (https://github.com/BurntSushi/ripgrep/releases)"
 	requirements="KO"
 else
 	requirements_status="ripgrep OK"
 fi
 
 if [[ $(nvim -v 2>&1 | grep "NVIM v" | cut -d'.' -f 2) -lt 8 || ! -f "/usr/bin/nvim" ]];then
-	requirements_status="/!\\ \033[1m$requirements_status\nNeoVim wrong version or missing (https://launchpad.net/~neovim-ppa/+archive/ubuntu/unstable)\033[0m"
+	requirements_status="$requirements_status\n/!\\ \033[1mNeoVim\033[0m wrong version or missing (https://launchpad.net/~neovim-ppa/+archive/ubuntu/unstable)"
 	requirements="KO"
 else
 	requirements_status="$requirements_status\nNeoVim OK"
 fi
 
 if [[ $(node -v 2>&1 | cut -d'.' -f1 | tail -c 3) -lt 18 || ! -f "/usr/bin/node" ]];then
-	requirements_status="/!\\ \033[1m$requirements_status\nNode wrong version or missing (https://joshtronic.com/2022/04/24/how-to-install-nodejs-18-on-ubuntu-2004-lts/)\033[0m"
+	requirements_status="$requirements_status\n/!\\ \033[1mNode\033[0m wrong version or missing (https://joshtronic.com/2022/04/24/how-to-install-nodejs-18-on-ubuntu-2004-lts/)"
         requirements="KO"
 else
         requirements_status="$requirements_status\nNode OK"
 fi
 
 if [[ ! -f "/usr/bin/npm" ]];then
-	requirements_status="/!\\ \033[1m$requirements_status\nnpm wrong version or missing (https://joshtronic.com/2022/04/24/how-to-install-nodejs-18-on-ubuntu-2004-lts/)\033[0m"
+	requirements_status="$requirements_status\n/!\\ \033[1mnpm\033[0m wrong version or missing (https://joshtronic.com/2022/04/24/how-to-install-nodejs-18-on-ubuntu-2004-lts/)"
         requirements="KO"
 else
         requirements_status="$requirements_status\nnpm OK"
 fi
 
 if [[ $(php -v 2>&1 | head -n 1 | cut -d' ' -f2 | cut -d'.' -f1) -lt 8 || ! -f "/usr/bin/php" ]];then
-	requirements_status="/!\\ \033[1m$requirements_status\nPHP wrong version or missing https://linuxize.com/post/how-to-install-php-8-on-ubuntu-20-04/)/!\\ \033[0m"
+	requirements_status="$requirements_status\n/!\\ \033[1mPHP\033[0m wrong version or missing https://linuxize.com/post/how-to-install-php-8-on-ubuntu-20-04/)"
         requirements="KO"
 else
         requirements_status="$requirements_status\nPHP OK"
 fi
 
 if [[ ! -f "/usr/bin/composer" ]];then
-	requirements_status="/!\\ \033[1m$requirements_status\ncomposer wrong version or missing (https://getcomposer.org/download/)/!\\ \033[0m"
+	requirements_status="$requirements_status\n/!\\ \033[1mcomposer\033[0m wrong version or missing (https://getcomposer.org/download/)"
         requirements="KO"
 else
         requirements_status="$requirements_status\ncomposer OK"
