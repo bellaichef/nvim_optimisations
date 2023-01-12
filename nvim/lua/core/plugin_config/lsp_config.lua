14694cefe04364674f9729d4bfe6115229b59662
bellaichef <45790724+bellaichef@users.noreply.github.com>
2023-01-12 09:37:12 +0100

    Update lsp_config.lua

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -48,3 +48,19 @@ require('lspconfig').bashls.setup {
   capabilities = capabilities,
 }
 
+require'lspconfig'.html.setup {
+	capabilities = capabilities,
+  on_attach = on_attach,
+	cmd = { "vscode-html-language-server", "--stdio" },
+	filetypes = { "html" },
+	init_options = {
+		configurationSection = { "html", "css", "javascript" },
+		embeddedLanguages = {
+			css = true,
+			javascript = true
+		},
+		provideFormatter = true
+	},
+	settings = {},
+	single_file_support = true
+}
